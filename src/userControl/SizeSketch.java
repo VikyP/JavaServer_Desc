65c7bacd47a36bcdf4a9183286b9f9ba61a0804d
Viky <Viky@Oleg_Acer>
2016-01-04 04:40:20 +0200

    new commit

---
+++ src/userControl/SizeSketch.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userControl;
+
+/**
+ *
+ * @author Viky
+ */
+public class SizeSketch
+{   
+    public final static int BUTTON_WIDTH=20; 
+    public final static int SPINNER_WIDTH=35;
+    
+    public final static int TOOLPANEL_WIDTH=170;
+    
+    public final static int ROW_WIDTH=TOOLPANEL_WIDTH-20;
+    public final static int TEXTBOX_WIDTH=ROW_WIDTH-BUTTON_WIDTH-20;
+    
+    public final static int COMBOBOX_WIDTH=ROW_WIDTH-6;
+    public final static int LABEL_WIDTH=(ROW_WIDTH-20)/2;
+    public final static int CONTROL_HEIGHT=22;
+    public final static int LISTBOX_HEIGHT=80;
+    public final static int ROW_HEIGHT=28;
+    
+}
